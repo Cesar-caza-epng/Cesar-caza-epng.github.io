03e72b6171e7b138e80a80243947dce1680a0317
Cesar-caza-epng <159482256+Cesar-caza-epng@users.noreply.github.com>
2024-04-19 18:11:16 -0600

    CYPU

--- PROYECTOS_PERSONALES/Cyberpuerta.js
+++ PROYECTOS_PERSONALES/Cyberpuerta.js
@@ -1,31 +1,34 @@
 center=document.querySelector("body");
 ram=document.getElementById("mejrsram");
 fre=document.getElementById("mjrfre");
-
+mjr=document.getElementById("mjr");
 function getpc(){
     fetch("./PY_webScrapp/Cyberpuerta.json").then((res)=>res.json()).then((data)=>seejson(data));
 }
 
 function seejson(data){
     let i=0;
+    let arrei;
+    let presio;
+    let hertz;
+    let gbspace;
     for (let key in data.PCs) {
-        h1=document.createElement("h1");
-        h1.innerHTML=`${key}`
-        center.appendChild(h1);
-        lista=document.createElement("ul");
          for(comp in data.PCs[key]){
             elemlist=document.createElement("li");
-            elemlist.innerHTML=`${comp}     ${data.PCs[key][comp]}`
-            lista.appendChild(elemlist);
             if(comp=="Memoria interna"){
-                let arrei=Object.values(data.PCs[key])[i].split(" ");
-                let presio=Object.values(data.PCs[key])[0];
+                arrei=parseInt(Object.values(data.PCs[key])[i]);
+                presio=Object.values(data.PCs[key])[0];
                 presio=presio.replace(/[,.]/g,"");
                 presio=presio.slice(0,-2);
                 presio=presio.slice(1);
                 presio=+presio;
-                if(presio<6000 && arrei[0]==16){
-                    elemlist.innerHTML=`${key}, Precio: $${presio}`
+                if(presio<6000 && arrei>=16){
+                    elemlist.innerHTML=`${key}, Precio: $${presio}<br><br>`;
+                    elemlist.classList.add("list-group-item");
+                    link=document.createElement("a");
+                    link.href=`https://www.cyberpuerta.mx/Computadoras/PC-s-de-Escritorio/${key}.html`
+                    link.innerHTML="Link de Pc"
+                    elemlist.appendChild(link);
                     ram.appendChild(elemlist);
                 }
             }
@@ -35,16 +38,34 @@ function seejson(data){
                 presio=presio.slice(0,-2);
                 presio=presio.slice(1);
                 presio=+presio;
-                let hertz=parseFloat(Object.values(data.PCs[key])[i]);
-                if(presio<6000 && hertz>3.6){
-                    elemlist.innerHTML=`${key}  Precio: $${presio}`
+                hertz=parseFloat(Object.values(data.PCs[key])[i]);
+                if(presio<6000 && hertz>=3.6){
+                    elemlist.innerHTML=`${key}  Precio: $${presio}<br><br>`
+                    elemlist.classList.add("list-group-item");
+                    link=document.createElement("a");
+                    link.href=`https://www.cyberpuerta.mx/Computadoras/PC-s-de-Escritorio/${key}.html`
+                    link.innerHTML="Link de Pc"
+                    elemlist.appendChild(link);
                     fre.appendChild(elemlist);
                 }
             }
+            if(comp=="Capacidad total de almacenaje"){
+                gbspace=Object.values(data.PCs[key])[i];
+                gbspace=parseInt(gbspace);
+            }
+
             i++;
          }
+         if(arrei>15 && presio<7000 && hertz>3.7 && gbspace>499){
+            elemlist.innerHTML=`${key} Precio: $${presio}<br><br>`
+            elemlist.classList.add("list-group-item");
+            link=document.createElement("a");
+            link.href=`https://www.cyberpuerta.mx/Computadoras/PC-s-de-Escritorio/${key}.html`
+            link.innerHTML="Link de Pc"
+            elemlist.appendChild(link);
+            mjr.appendChild(elemlist);
+         }
          i=0;
-        center.appendChild(lista);
     }
 }
 
