a17b07d446bc51028da49b7881c6e24b10a6a513
Cesar-caza-epng <159482256+Cesar-caza-epng@users.noreply.github.com>
2024-04-30 02:18:44 -0600

    lo0

--- GamesTorrent/WebScrappingGames/games.py
+++ GamesTorrent/WebScrappingGames/games.py
@@ -4,30 +4,38 @@ import json
 from unidecode import unidecode
 import re
 
-url=requests.get("https://steam250.com/top250")
+url=requests.get("https://steam250.com/top250") #conseguimos el contenido html
+data={}
+content=BeautifulSoup(url.content,"html.parser")#lo transformamos en contenido que se pueda utilizar
 
-content=BeautifulSoup(url.content,"html.parser")
-
-spans=content.find_all("span", class_="title")
+spans=content.find_all("span", class_="title") #ecnontramos adentro de el contenido todas las etiquetas span con la clase title (nos regresa una lista)
 names=[]
 urls=[]
-for span in spans:
+imgurls=[]
+for span in spans: #por cada span en spans, se busca una etiqueta de link y luego se consigue su atributo href y tambien se apendea el nombre en forma de str a la lista names
     name=span.find("a")
-
     if name:
-        if name!="":
-            urls.append(name["href"])
-            names.append(name.text.strip())
+        urls.append(name["href"])
+        names.append(name.text.strip())
+#esto se usa porque la pagina a la que queremos acceder se mamonea y necesita saber q no queremos hacer nada malicioso
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1 Safari/605.1.15'
 }
-url2 = requests.get("https://club.steam250.com/app/620", headers=headers)
-content2=BeautifulSoup(url2.content, "html.parser")
-div=content2.find("div", class_="logo")
-style=div["style"]
-urlimg = re.findall(r'\(([^)]+)\)', style)
 
-print(urlimg)
+names=list(filter(None, names)) #se filtra la lista porque por alguna razon salen varios elementos nulos
+data["names"]=names
+for url in urls:
+    aurl=requests.get(url, headers=headers)
+    print(aurl)
+    conten=BeautifulSoup(aurl.content, "html.parser")
+    div=conten.find("div", class_="logo")
+    estail=div["style"]
+    imgurls.append(re.findall(r'\(([^)]+)\)', estail))
 
+    print("vuelta terminada")
 
+imgurls=list(filter(None, imgurls))
+data["imgurls"]=imgurls
 
+with open("Steamgames.json", "w") as file:
+    json.dump(data,file,indent=4)
